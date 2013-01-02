b2758f1a6959b25aadf7c6f199f96e1a95b9054e
Sasan <sasan@commeng.org>
2013-01-02 00:08:26 -0800

    * Initial

---
+++ static/js/gooyand.js
@@ -0,0 +1,21 @@
+$(function(){
+  $('.link-container').hover(function() {
+    $(this).addClass('hover');
+  }, function() {
+    $(this).removeClass('hover');
+  });
+
+
+  $('.order-by-popularity').tooltip({
+    title: 'Sort by popularity'
+  });
+
+  $('.order-by-alpha').tooltip({
+    title: 'Sort by name'
+  });
+
+  // Don't jump
+  $('a[href="#"]').click(function(ev) {
+    ev.preventDefault();
+  });
+});
