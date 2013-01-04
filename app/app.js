e0d4ec5cf2aa9514e25341fe50170ad33beafbd3
Sasan <sasan@commeng.org>
2013-01-03 22:53:54 -0800

    meta tags

--- app/app.js
+++ app/app.js
@@ -16,11 +16,13 @@ var app = express()
   ;
 
 
+app.disable('x-powered-by');
+
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
+  app.use(express.favicon(__dirname + '/public/favicon.ico', {maxAge: 2592000000}));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
@@ -50,13 +52,18 @@ app.post('/click', function(req, res) {
   var id = req.body.id
     , ip = req.headers['X-Forwarded-For'];
 
+
   linkProvider.addClick(id, ip, function(err) {
 
     res.send({});
   });
 });
 
-app.get('/users', user.list);
+app.get('/setup', function(req, res) {
+  linkProvider.importLinks();
+
+  res.send('Done');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
